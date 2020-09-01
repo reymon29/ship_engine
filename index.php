1622a8d6a91d36eac784ce2f3e699daf04d41688
Reyes Montemayor <sjpete1234@hotmail.com>
2020-09-01 00:11:19 -0400

    fedex carrier test

--- index.php
+++ index.php
@@ -1,5 +1,56 @@
 <?php
   include "post_ship_engine.php";
 
-  print_r($data);
- ?>
+  // pass an array in ship engine format
+  $data_array =  array(
+    "shipment" => [
+      "service_code" => "fedex_home_delivery",
+      "ship_to" => [
+        "name" => "Jane Doe",
+        "address_line1" => "525 S Winchester Blvd",
+        "city_locality" => "San Jose",
+        "state_province" => "CA",
+        "postal_code" => "95128",
+        "country_code" => "US",
+        "address_residential_indicator" => "yes"
+      ],
+      "ship_from" => [
+        "name" => "John Doe",
+        "company_name" => "Example Corp",
+        "phone" => "555-555-5555",
+        "address_line1" => "4009 Marathon Blvd",
+        "city_locality" => "Austin",
+        "state_province" => "TX",
+        "postal_code" => "78756",
+        "country_code" => "US",
+        "address_residential_indicator" => "no"
+      ],
+      "packages" => [
+        [
+          "weight" => [
+            "value" => 20,
+            "unit" => "ounce"
+          ],
+          "dimensions" => [
+            "height" => 6,
+            "width" => 12,
+            "length" => 24,
+            "unit" => "inch"
+          ]
+        ]
+      ]
+    ]
+  );
+
+  // Call the API with passing the array of shipping data
+  $make_call = callAPI('POST', 'https://api.shipengine.com/v1/labels/', json_encode($data_array));
+  $response = json_decode($make_call, true);
+  $errors   = $response['response']['errors'];
+  $data     = $response;
+
+  // Return response to page
+  echo json_encode($data);
+  // return the pdf url to the label created
+  $print_data = $_POST[$data["label_download"]["pdf"]];
+
+?>
--- post_ship_engine.php
+++ post_ship_engine.php
@@ -20,7 +20,7 @@ function callAPI($method, $url, $data){
    // OPTIONS:
    curl_setopt($curl, CURLOPT_URL, $url);
    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'API-Key: Enter API Key1',
+      'API-Key: XXXAPIKEYXXXX',
       'Content-Type: application/json',
    ));
    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -32,49 +32,5 @@ function callAPI($method, $url, $data){
    return $result;
 }
 
-$data_array =  array(
-  "shipment" => [
-    "service_code" => "ups_ground",
-    "ship_to" => [
-      "name" => "Jane Doe",
-      "address_line1" => "525 S Winchester Blvd",
-      "city_locality" => "San Jose",
-      "state_province" => "CA",
-      "postal_code" => "95128",
-      "country_code" => "US",
-      "address_residential_indicator" => "yes"
-    ],
-    "ship_from" => [
-      "name" => "John Doe",
-      "company_name" => "Example Corp",
-      "phone" => "555-555-5555",
-      "address_line1" => "4009 Marathon Blvd",
-      "city_locality" => "Austin",
-      "state_province" => "TX",
-      "postal_code" => "78756",
-      "country_code" => "US",
-      "address_residential_indicator" => "no"
-    ],
-    "packages" => [
-      [
-        "weight" => [
-          "value" => 20,
-          "unit" => "ounce"
-        ],
-        "dimensions" => [
-          "height" => 6,
-          "width" => 12,
-          "length" => 24,
-          "unit" => "inch"
-        ]
-      ]
-    ]
-  ]
-);
-
-$make_call = callAPI('POST', 'https://api.shipengine.com/v1/labels/', json_encode($data_array));
-$response = json_decode($make_call, true);
-$errors   = $response['response']['errors'];
-$data     = $response;
 
 ?>
